d658384ee0e831b89f0cea193f464796962a8fc4
Ciibelle <85749588+Ciibelle@users.noreply.github.com>
2022-10-04 12:15:29 -0300

    Add files via upload

---
+++ calcSalario.php
@@ -0,0 +1,7 @@
+<?php
+   $valor = $_GET['txtValor'];
+   $hora = $_GET['txtHora'];
+   $gratificacao = $_GET['txtGratificacao'];
+   $saldo = $hora * $valor + $gratificacao;
+   echo "O Salário é: " . $saldo;
+?>
